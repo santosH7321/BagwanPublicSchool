c08b4e7263248e8919c7d549f5444a8470fe37b6
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-06 13:00:45 +0530

    auth

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
+import { User } from "lucide-react"
+export default function Home() {
+  return(
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
